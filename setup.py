7451a8b6b73e4dd69a031e121c380bab4ca34efe
André Rafael <andrerafael2000@gmail.com>
2022-05-12 10:57:20 -0300

    Preparar para package

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+with open("requeriment.txt", "r", encoding="utf-8") as fh:
+    requeriments = fh.read().splitlines()
+
 setuptools.setup(
     name='my_tools',
     version='1.0',
@@ -10,5 +13,8 @@ setuptools.setup(
     author_email='andre_rafael2012@hotmail.com',
     description='The tools I use every day',
     long_description=long_description,
-    url=''
+    url='https://github.com/Andre-Rafael/my_tools',
+    license='MIT',
+    packages=['my_tools'],
+    requires=requeriments
 )
